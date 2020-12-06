d8d2ead2f87eb0b17b16eae273f4d8e5ef260b02
Taner MUSTAFA <taner.mustafa@stud.acs.upb.ro>
2020-12-06 14:00:46 -0800

    SPRC Tema2 Endpoint1

--- SPRC/Teme/Tema2/src/main.py
+++ SPRC/Teme/Tema2/src/main.py
@@ -1,4 +1,6 @@
 import sqlalchemy as db
+import pandas as pd
+import json
 from flask import Flask
 from flask import request, jsonify, Response
 
@@ -6,6 +8,24 @@ app = Flask(__name__)
 
 global engine, connection, metadata
 global tari, orase, temperaturi
+
+NUME = "nume"
+LAT = "lat"
+LON = "lon"
+ID = "id"
+IDTARA = "idTara"
+IDORAS = "idOras"
+VALOARE = "valoare"
+TIMESTAMP = "timestamp"
+
+NUME_TARA = "nume_tara"
+LATITUDINE = "latitudine"
+LONGITUDINE = "longitudine"
+ID_TARA = "id_tara"
+NUME_ORAS = "nume_oras"
+ID_ORAS = "id_oras"
+VALOARE = "valoare"
+TIME_STAMP = "time_stamp"
 
 def connect_to_db():
     global engine, connection, metadata
@@ -26,9 +46,9 @@ def create_db():
     database_creation_command = """
         CREATE TABLE IF NOT EXISTS tari(
             id INT(5) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-            nume_tara varchar(50) UNIQUE,
-            latitudine DOUBLE(20,20),
-            longitudine DOUBLE(20,20)
+            nume_tara varchar(50) UNIQUE NOT NULL,
+            latitudine DOUBLE,
+            longitudine DOUBLE
         );
     """
     connection.execute(database_creation_command)
@@ -36,12 +56,12 @@ def create_db():
     database_creation_command = """
     CREATE TABLE IF NOT EXISTS orase(
         id INT(5) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        id_tara INT(5),
-        nume_oras varchar(50),
-        latitudine DOUBLE(20,20),
-        longitudine DOUBLE(20,20),
+        id_tara INT(5) NOT NULL,
+        nume_oras varchar(50) NOT NULL,
+        latitudine DOUBLE,
+        longitudine DOUBLE,
         
-        FOREIGN KEY (id_tara) REFERENCES tari(id),
+        FOREIGN KEY (id_tara) REFERENCES tari(id) ON DELETE CASCADE,
         UNIQUE KEY unique_index (id_tara, nume_oras)
     );
     """
@@ -50,11 +70,11 @@ def create_db():
     database_creation_command = """
     CREATE TABLE IF NOT EXISTS temperaturi(
         id INT(5) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        id_oras INT(5),
-        valoare INT(5),
+        id_oras INT(5) NOT NULL,
+        valoare INT(5) NOT NULL,
         time_stamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
         
-        FOREIGN KEY (id_oras) REFERENCES orase(id),
+        FOREIGN KEY (id_oras) REFERENCES orase(id) ON DELETE CASCADE,
         UNIQUE KEY unique_index (id_oras, time_stamp)
     );
     """
@@ -66,9 +86,47 @@ def create_db():
     orase = db.Table('orase', metadata, autoload=True, autoload_with=engine)
     temperaturi = db.Table('temperaturi', metadata, autoload=True, autoload_with=engine)
 
+@app.route("/api/countries", methods=["POST"])
+def req1():
+    global tari
+    global connection
+
+    payload = request.get_json()
+    if not payload:
+        return Response(status=400)
+    try:
+        nume = payload[NUME]
+        lat = payload[LAT]
+        lon = payload[LON]
+
+        int(lat)
+        int(lon)
+        if str(nume) == 0:
+            raise
+    except:
+        return Response(status=400)
+
+    try:
+        query = db.insert(tari).values(nume_tara=nume, latitudine=lat, longitudine=lon)
+        connection.execute(query)
+    except:
+        return Response(status=409)
+
+    last_insert = connection.execute(db.select([tari])).fetchall()
+    id = int(last_insert[-1].id)
+
+    response = app.response_class(
+        response=json.dumps({"id": id}),
+        status=201,
+        mimetype='application/json'
+    )
+    return response
+
 def main():
     connect_to_db()
     create_db()
+
+    print(repr(metadata.tables['orase']))
 
 if __name__ == "__main__":
     main()
