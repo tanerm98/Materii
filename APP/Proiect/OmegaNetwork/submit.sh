5cf0530a1b926eed1578fc76de29abcc5c16a85c
Taner Mustafa <tmustafa@fitbit.com>
2020-12-14 01:02:15 +0200

    APP Proiect OpenMP P=1 bug

--- APP/Proiect/OmegaNetwork/submit.sh
+++ APP/Proiect/OmegaNetwork/submit.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-qsub -q ibm-nehalem.q -cwd ./run.sh
-qstat
+#qsub -q ibm-nehalem.q -cwd ./run.sh
+#qstat
 
 #qsub -q ibm-nehalem.q -pe openmpi 16 -cwd ./run.sh
-#qstat
+#qst
+
+qsub -q ibm-nehalem.q -pe openmpi*16 16 -cwd ./run.sh
